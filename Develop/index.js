33b5c40f3b9c3eeb879c26dd682ae28b2c56f689
Lyne Malele <lynemalele@gmail.com>
2020-10-09 07:00:04 -0400

    Completed user questions. Called the reame file function. Called function to initialize the program

--- Develop/index.js
+++ Develop/index.js
@@ -1,65 +1,91 @@
+// need the const fs variable here
+const fs = require("fs");
+
+// need inquirer variable here
 const inquirer = require("inquirer");
+
+// need a markdown js file here
 const generateMarkdown = require("./utils/generateMarkdown")
 
 // require fs npm
 
 // array of questions for user
 const questions = [
+   
     {
-        message: "What is your Github username?",
-        name: "Github"
-    },
-    {
-        message: "What is your email address?",
-        name: "email"
-    },
-    {
+        type: "input",
         message: "What is your project title?",
-        name: "title"
+        name: "Title"
     },
     {
-        title: "Description",
-        message: "What is your project description?",
-        name: "description"
+        type: "input",
+        message: "Please enter a description of your project:",
+        name: "Description"
+    },
+   // Table of Contents, adding this in the markdown.js
+    {
+        type: "input",
+        message: "What are the steps required to install your program?",
+        name: "Installation"
     },
     {
-        message: "What are the installation instructions?",
-        name: "installation"
+        type: "input",
+        message: "Please provide the usage instructions:",
+        name: "Usage"
     },
     {
-        message: "What is the usage information?",
-        name: "usage"
+        type: "input",
+        message:  "Which license are you using for this project?",
+        name: "License",
     },
     {
-        message: "Who are the contributors of this project?",
-        name: "contributing"
+        type: "input",
+        message: "How can users contribute to this project?",
+        name: "Contributing"
     },
     {
-        message: "Which tests will you run?",
-        name: "tests"
+        type: "input",
+        message: "Please provide the test instructions:",
+        name: "Tests"
     },
+
+    // QUESTIONS section -- github username
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "userName"
+    },
+    // QUESTIONS section -- email address
+    {
+        type: "input",
+        message: "What is your Github email address?",
+        name: "Questions"
+    },
+    
+   
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    return false.writeFileSync(path.join(process.cwd()))
+   fs.writeFile(fileName, generateMarkdown(data), function (err) {
+       if(err) {
+           return console.log(err);
+       }
+   });
 }
 
 // function to initialize program
 function init() {
-
-    // ask user questions about project
+// ask user questions about project
     inquirer.prompt(questions)
         .then((answers) => {
-            console.log(answers);
-            const markDown = generateMarkdown(answers);
-            console.log(markDown);
+            console.log(JSON.stringify(answers, null, " "));
+            answers.getLicense = getLicense(answers.license);
+            // save string to a .md file
+            writeToFile("README.md", answers);
         })
-    console.log("After questions!")
-
+    //console.log("After questions!")
     
-    // save string to a .md file
-    writeToFile("README")
     //fs.appendFile()
 }
 
